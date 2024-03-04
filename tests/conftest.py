812c8d1bcd541f9936a11add43d8510e3937a6ad
Evgeny Zelenin <evgeny.globe@yandex.ru>
2024-03-04 15:21:51 +0300

    Сделал правки

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,6 @@ from selene import browser
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_options():
-    browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = 1440
     browser.config.window_height = 1860
 
